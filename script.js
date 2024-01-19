98b35cdad894eac1204a5d03d67b3c02951fb3b9
Tennessee Foster <68438949+TennaThePhantom@users.noreply.github.com>
2024-01-19 00:58:52 -0500

    Finish menu and exiting & clean some code

--- script.js
+++ script.js
@@ -6,23 +6,45 @@ const colorGreen = document.querySelector(".green");
 const colors = [colorRed, colorBlue, colorYellow, colorGreen];
 let colorSequence = [];
 
+// this removes the start menu before you start
+function startScreen() {
+	const startButton = document.getElementById("startButton");
+	const startMenu = document.querySelector(".start-screen");
+	const container = document.querySelector(".container");
 
+	const handleMouseOut = () => {
+		for (const color of colors) {
+			color.style.pointerEvents = "none";
+		}
+	};
+
+	container.addEventListener("mouseout", handleMouseOut);
+
+	startButton.addEventListener("click", () => {
+		startMenu.style.display = "none";
+		container.style.opacity = "1";
+		container.removeEventListener("mouseout", handleMouseOut);
+		for (const color of colors) {
+			color.style.pointerEvents = "auto";
+		}
+	});
+}
+
+startScreen();
 // gets the first color for colorSequence
 function firstColorPick() {
 	const min = 0;
 	const max = 3;
 	let randomFirstColor = Math.floor(Math.random() * (max - min + 1)) + min;
-    let randomColor = colors[randomFirstColor];
-    let colorClassName = randomColor.className;
+	let randomColor = colors[randomFirstColor];
+	let colorClassName = randomColor.className;
 	console.log("The first color is " + colorClassName);
-    colorSequence.push(colorClassName);
+	colorSequence.push(colorClassName);
 }
 firstColorPick();
 
 /*
 if person gets the first color correct this function actives to add another color to colorSequence array
 */
-function pickColorRandomly(){
-
-}
-console.log(colorSequence)
+function pickColorRandomly() {}
+console.log(colorSequence);
