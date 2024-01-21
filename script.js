0d10bc7b2f36433feaf273cc65efc8ec00004646
Tennessee Foster <68438949+TennaThePhantom@users.noreply.github.com>
2024-01-20 23:12:53 -0500

    color light ups on it's on without hover

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const colorGreen = document.querySelector(".green");
 const colors = [colorRed, colorBlue, colorYellow, colorGreen];
 let colorSequence = [];
 
+let activeBrighter = false;
+
 // this removes the start menu before you start
 function startScreen() {
 	const startButton = document.getElementById("startButton");
@@ -27,10 +29,17 @@ function startScreen() {
 		for (const color of colors) {
 			color.style.pointerEvents = "auto";
 		}
+		activeBrighter = true;
+
+		if (activeBrighter === true) {
+			firstColorPick();
+			console.log(colorSequence);
+		}
 	});
 }
 
 startScreen();
+
 // gets the first color for colorSequence
 function firstColorPick() {
 	const min = 0;
@@ -40,11 +49,27 @@ function firstColorPick() {
 	let colorClassName = randomColor.className;
 	console.log("The first color is " + colorClassName);
 	colorSequence.push(colorClassName);
+	switch (colorClassName) {
+		case "green":
+			colorGreen.classList.add("bright-up-green");
+			break;
+		case "red":
+			colorRed.classList.add("bright-up-red");
+			break;
+		case "blue":
+			colorBlue.classList.add("bright-up-blue");
+
+			break;
+		case "yellow":
+			colorYellow.classList.add("bright-up-yellow");
+
+			break;
+		default:
+			break;
+	}
 }
-firstColorPick();
 
 /*
 if person gets the first color correct this function actives to add another color to colorSequence array
 */
 function pickColorRandomly() {}
-console.log(colorSequence);
